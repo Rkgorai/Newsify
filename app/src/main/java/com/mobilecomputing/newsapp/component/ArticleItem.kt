c14b12963a726a127a14fdb00d6abdac0115d896
Rkgorai <rahul.k.gorai@gmail.com>
2024-04-26 12:25:48 +0530

    Some minor Change in DateFormat

--- app/src/main/java/com/mobilecomputing/newsapp/component/ArticleItem.kt
+++ app/src/main/java/com/mobilecomputing/newsapp/component/ArticleItem.kt
@@ -19,6 +19,9 @@ import coil.compose.AsyncImage
 //import com.google.accompanist.imageloading.rememberImagePainter
 import com.mobilecomputing.newsapp.R
 import com.mobilecomputing.newsapp.model.news.Article
+import java.util.*;
+import java.text.SimpleDateFormat
+
 
 @Composable
 fun ArticleItem(article: Article) {
@@ -28,21 +31,25 @@ fun ArticleItem(article: Article) {
         elevation = CardDefaults.elevatedCardElevation(8.dp)
     ) {
         Column(modifier = Modifier.padding(16.dp)) {
-            Text(text = article.title ?: "", style = MaterialTheme.typography.titleLarge)
-            Spacer(modifier = Modifier.height(8.dp))
-
             // Add the image here
-
             AsyncImage(
                 model = article.urlToImage ?: "",
                 placeholder = painterResource(id = R.drawable.sudoimage),
                 error = painterResource(id = R.drawable.sudoimage),
                 contentDescription = "The delasign logo",
             )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = article.title ?: "", style = MaterialTheme.typography.titleLarge)
 
             Spacer(modifier = Modifier.height(16.dp))
             Text(text = "Author: ${article.author}", fontSize = 14.sp)
-            Text(text = "Published At: ${article.publishedAt}", fontSize = 14.sp)
+
+            val inputFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.getDefault())
+            val outputFormat = SimpleDateFormat("dd MMM yyyy, HH:mm a", Locale.getDefault())
+            val date: Date? = inputFormat.parse(article.publishedAt)
+            val formattedDate: String? = date?.let { outputFormat.format(it) }
+
+            Text(text = "Published At: ${formattedDate}", fontSize = 14.sp)
             Text(text = "Source: ${article.source.name}", fontSize = 14.sp)
             Spacer(modifier = Modifier.height(8.dp))
         }
