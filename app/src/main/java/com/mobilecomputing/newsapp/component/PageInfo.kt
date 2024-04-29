c4af964f75fe7fc96b81144cadd922ae4a8b8c87
mukul <mukulvyas19@gmail.com>
2024-04-29 20:26:06 +0530

    Added New UI to pageInfo

--- app/src/main/java/com/mobilecomputing/newsapp/component/PageInfo.kt
+++ app/src/main/java/com/mobilecomputing/newsapp/component/PageInfo.kt
@@ -1,29 +1,46 @@
 package com.mobilecomputing.newsapp.component
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
 import androidx.core.content.ContextCompat
 import coil.compose.AsyncImage
 import com.mobilecomputing.newsapp.R
@@ -45,68 +62,129 @@ fun ArticleItemPage(
     source: String?,
     context: Context
 ) {
+    val activity = context as? Activity
     val scrollState = rememberScrollState(0)
 
+    BackHandler {
+        activity?.finish()
+    }
+
+
     Column(
-        modifier = Modifier.padding(16.dp)
+        modifier = Modifier
             .fillMaxSize()
             .verticalScroll(scrollState)
     ) {
-        // Add the image here
-        AsyncImage(
+
+
+        Box(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(200.dp),
-            model = urlToImage ?: "",
-            placeholder = painterResource(id = R.drawable.sudoimage),
-            error = painterResource(id = R.drawable.sudoimage),
-            contentDescription = "The delasign logo",
-        )
-        Spacer(modifier = Modifier.height(8.dp))
-        Text(text = title ?: "", style = MaterialTheme.typography.headlineLarge)
+                .height(200.dp)
+        ) {
+            AsyncImage(
+                modifier = Modifier
+                    .fillMaxSize(),
+                model = urlToImage ?: "",
+                placeholder = painterResource(id = R.drawable.sudoimage),
+                error = painterResource(id = R.drawable.sudoimage),
+                contentDescription = "The delasign logo",
+                contentScale = ContentScale.FillBounds
+            )
 
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(text = "Author: $author", fontSize = 16.sp)
+            IconButton(
+                onClick = { activity?.finish() },
+                modifier = Modifier
+                    .padding(7.dp)
+                    .align(Alignment.TopStart)
+                    .background(Color.White.copy(alpha = 0.6f), shape = CircleShape)
+                    .zIndex(1f)
+                    .absoluteOffset(y = 0.dp)
 
-        val inputFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.getDefault())
-        val outputFormat = SimpleDateFormat("dd MMM yyyy, HH:mm a", Locale.getDefault())
-        val date: Date? = publishedAt?.let { inputFormat.parse(it) }
-        val formattedDate: String? = date?.let { outputFormat.format(it) }
-
-        Text(text = "Published At: $formattedDate", fontSize = 16.sp)
-        Text(text = "Source: $source", fontSize = 16.sp)
-        Spacer(modifier = Modifier.height(8.dp))
-        Text(text = "Description:", style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold))
-        Text(text ="$description", fontSize = 18.sp)
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(text = "Content:", style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold))
-        Text(text = "$content", fontSize = 18.sp)
-
-        // Add the link here
-        val annotatedString = buildAnnotatedString {
-            withStyle(style = SpanStyle(textDecoration = TextDecoration.Underline)) {
-                append("Read from Source")
-                addStringAnnotation(
-                    tag = "URL",
-                    annotation = url ?: "",
-                    start = 0,
-                    end = "Read from Source".length
-                )
+            ) {
+                Icon(Icons.Filled.ArrowBack, contentDescription = "Back", tint = Color.Black)
             }
         }
 
-        ClickableText(
-            text = annotatedString,
-            onClick = { offset ->
-                annotatedString.getStringAnnotations(tag = "URL", start = offset, end = offset)
-                    .firstOrNull()?.let { annotation ->
-                        val intent = Intent(context, WebViewActivity::class.java).apply {
-                            putExtra("url", annotation.item)
-                        }
-                        ContextCompat.startActivity(context, intent, null)
-                    }
-            },
-            modifier = Modifier.padding(top = 16.dp)
-        )
+
+
+        Spacer(modifier = Modifier.height(8.dp))
+//        Text(text = title ?: "", style = MaterialTheme.typography.headlineLarge)
+//
+//        Spacer(modifier = Modifier.height(16.dp))
+//        Text(text = "Author: $author", fontSize = 16.sp)
+//
+//        val inputFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.getDefault())
+//        val outputFormat = SimpleDateFormat("dd MMM yyyy, HH:mm a", Locale.getDefault())
+//        val date: Date? = publishedAt?.let { inputFormat.parse(it) }
+//        val formattedDate: String? = date?.let { outputFormat.format(it) }
+//
+//        Text(text = "Published At: $formattedDate", fontSize = 16.sp)
+//        Text(text = "Source: $source", fontSize = 16.sp)
+      //  Spacer(modifier = Modifier.height(8.dp))
+      Column(modifier = Modifier.padding(16.dp)) {
+
+          Box(
+              modifier = Modifier
+                  .border(5.dp, Color.Black)
+                  .padding(16.dp)
+          ) {
+              Column(
+                  modifier = Modifier.align(Alignment.Center)
+              ) {
+                  Text(text = title ?: "", style = MaterialTheme.typography.headlineMedium)
+
+                  Spacer(modifier = Modifier.height(16.dp))
+                  Row(
+                      modifier = Modifier.fillMaxWidth()
+                  ) {
+                      if (author != null) {
+                          Text(text = "Author: $author", fontSize = 16.sp, modifier = Modifier.weight(1f))
+                      }
+
+                      val inputFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.getDefault())
+                      val outputFormat = SimpleDateFormat("dd MMM yyyy, HH:mm a", Locale.getDefault())
+                      val date: Date? = publishedAt?.let { inputFormat.parse(it) }
+                      val formattedDate: String? = date?.let { outputFormat.format(it) }
+
+                      Text(text = "$formattedDate", fontSize = 14.sp, modifier = Modifier.weight(1f), textAlign = TextAlign.End)
+                  }
+//                Text(text = "Source: $source", fontSize = 14.sp)
+                  //Spacer(modifier = Modifier.height(8.dp))
+              }
+          }
+          Text(text = "Description:", style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold))
+          Text(text ="$description", fontSize = 18.sp)
+          Spacer(modifier = Modifier.height(16.dp))
+          Text(text = "Content:", style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold))
+          Text(text = "$content", fontSize = 18.sp)
+
+          // Add the link here
+          val annotatedString = buildAnnotatedString {
+              withStyle(style = SpanStyle(textDecoration = TextDecoration.Underline)) {
+                  append("Read from Source")
+                  addStringAnnotation(
+                      tag = "URL",
+                      annotation = url ?: "",
+                      start = 0,
+                      end = "Read from Source".length
+                  )
+              }
+          }
+
+          ClickableText(
+              text = annotatedString,
+              onClick = { offset ->
+                  annotatedString.getStringAnnotations(tag = "URL", start = offset, end = offset)
+                      .firstOrNull()?.let { annotation ->
+                          val intent = Intent(context, WebViewActivity::class.java).apply {
+                              putExtra("url", annotation.item)
+                          }
+                          ContextCompat.startActivity(context, intent, null)
+                      }
+              },
+              modifier = Modifier.padding(top = 16.dp)
+          )
+      }
     }
 }
--- app/src/main/java/com/mobilecomputing/newsapp/screens/news/SecondMainActivityScreen.kt
+++ app/src/main/java/com/mobilecomputing/newsapp/screens/news/SecondMainActivityScreen.kt
@@ -1,67 +1,96 @@
 package com.mobilecomputing.newsapp.screens.news
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.verticalScroll
 import android.app.Activity
 import android.content.Intent
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
+import coil.compose.AsyncImage
+import com.mobilecomputing.newsapp.R
 import com.mobilecomputing.newsapp.component.ArticleItemPage
-
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SecondMainActivityScreen(intent: Intent) { // Receive the intent as a parameter
     val context = LocalContext.current // Get the current context
     val activity = context as? Activity // Safe cast to Activity
+//    val urlToImage = intent.getStringExtra("urlToImage")
 
     // Handle the back button press
     BackHandler {
         activity?.finish()
     }
-    Column {
-        IconButton(
-            onClick = { activity?.finish() }, modifier = Modifier
-                .align(Alignment.Start)
-        ) {
-            Icon(Icons.Filled.ArrowBack, contentDescription = "Back", tint = Color.Black)
-        }
 
-        Box(
-            modifier = Modifier
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text(text = "") },
+                colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.Transparent),
+                //elevation = CardDefaults.cardElevation(defaultElevation = 5.dp),
 
-                .fillMaxSize()
-        ) {
-            // Add a back button at the top left corner
-
-            val title = intent.getStringExtra("title")
-            val content = intent.getStringExtra("content")
-            val description = intent.getStringExtra("description")
-            val author = intent.getStringExtra("author")
-            val url = intent.getStringExtra("url")
-            val urlToImage = intent.getStringExtra("urlToImage")
-            val publishedAt = intent.getStringExtra("publishedAt")
-            val source = intent.getStringExtra("source")
-
-            ArticleItemPage(
-                title,
-                content,
-                description,
-                author,
-                url,
-                urlToImage,
-                publishedAt,
-                source,
-                context
             )
+
+        }
+    ) {
+
+        Column {
+
+            Box(
+                modifier = Modifier
+
+                    .fillMaxSize()
+            ) {
+
+                val title = intent.getStringExtra("title")
+                val content = intent.getStringExtra("content")
+                val description = intent.getStringExtra("description")
+                val author = intent.getStringExtra("author")
+                val url = intent.getStringExtra("url")
+                val urlToImage = intent.getStringExtra("urlToImage")
+                val publishedAt = intent.getStringExtra("publishedAt")
+                val source = intent.getStringExtra("source")
+
+                ArticleItemPage(
+                    title,
+                    content,
+                    description,
+                    author,
+                    url,
+                    urlToImage,
+                    publishedAt,
+                    source,
+                    context
+                )
+            }
         }
     }
-
 }
+
+
+
