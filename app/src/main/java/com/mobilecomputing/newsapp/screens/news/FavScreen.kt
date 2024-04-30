e20e87b09428ab2707891c7659bc5129129d281c
mukul <mukulvyas19@gmail.com>
2024-04-30 19:22:59 +0530

    deleting news from favorite tab

--- app/src/main/java/com/mobilecomputing/newsapp/screens/news/FavScreen.kt
+++ app/src/main/java/com/mobilecomputing/newsapp/screens/news/FavScreen.kt
@@ -1,5 +1,12 @@
 package com.mobilecomputing.newsapp.screens.news
 
+import android.content.Context
+import android.content.Intent
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
@@ -8,16 +15,46 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import com.mobilecomputing.newsapp.SecondMainActivity
 import com.mobilecomputing.newsapp.component.ArticleItemSecond
 import com.mobilecomputing.newsapp.database.ArticleDao
 import com.mobilecomputing.newsapp.database.NewsArticle
 import kotlinx.coroutines.launch
+//
+//@Composable
+//fun FavScreen(dao: ArticleDao) {
+//    val scope = rememberCoroutineScope()
+//    val articles = remember { mutableStateOf(listOf<NewsArticle>()) }
+//
+//    LaunchedEffect(Unit) {
+//        scope.launch {
+//            articles.value = dao.getAllArticles()
+//        }
+//    }
+//
+//    if (articles.value.isEmpty()) {
+//        Text("No articles found")
+//    } else {
+//        LazyColumn {
+//            items(articles.value.reversed()) { article ->
+//                ArticleItemSecond(listOf(article))
+//            }
+//        }
+//    }
+//}
 
 @Composable
 fun FavScreen(dao: ArticleDao) {
     val scope = rememberCoroutineScope()
     val articles = remember { mutableStateOf(listOf<NewsArticle>()) }
 
+    val context = LocalContext.current // Get the current context
+
+    val startActivityLauncher = rememberLauncherForActivityResult(contract = ActivityResultContracts.StartActivityForResult()) { result ->
+        // Handle the result of the activity here
+    }
     LaunchedEffect(Unit) {
         scope.launch {
             articles.value = dao.getAllArticles()
@@ -29,7 +66,23 @@ fun FavScreen(dao: ArticleDao) {
     } else {
         LazyColumn {
             items(articles.value.reversed()) { article ->
-                ArticleItemSecond(listOf(article))
+                Box(modifier = Modifier.clickable {
+                    // Create an Intent to start the new activity
+                    val intent = Intent(context, SecondMainActivity::class.java)
+
+                    intent.putExtra("title", article.title)
+                    intent.putExtra("author", article.author)
+                    intent.putExtra("publishedAt", article.publishedAt)
+                    intent.putExtra("source", article.source)
+                    intent.putExtra("url", article.url)
+                    intent.putExtra("urlToImage", article.urlToImage)
+                    intent.putExtra("content", article.content)
+                    intent.putExtra("description", article.description)
+                    // Use the launcher to start the activity
+                    startActivityLauncher.launch(intent)
+                }) {
+                    ArticleItemSecond(listOf(article))
+                }
             }
         }
     }
