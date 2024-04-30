1a95efb2f530735ae46751d2cdeae210f3b9ebe1
Rkgorai <rahul.k.gorai@gmail.com>
2024-04-30 19:37:30 +0530

    Added Search Page

--- app/src/main/java/com/mobilecomputing/newsapp/screens/news/SearchScreen.kt
+++ app/src/main/java/com/mobilecomputing/newsapp/screens/news/SearchScreen.kt
@@ -1,8 +1,15 @@
 package com.mobilecomputing.newsapp.screens.news
 
+import android.content.Intent
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
@@ -12,18 +19,32 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.mobilecomputing.newsapp.SecondMainActivity
+import com.mobilecomputing.newsapp.component.ArticleItem
+import com.mobilecomputing.newsapp.model.news.Article
 
 @Composable
-fun SearchScreen() {
+fun SearchScreen(viewModel: NewsViewModel = hiltViewModel()) {
     val searchQuery = remember { mutableStateOf("") }
 
+    val context = LocalContext.current // Get the current context
+
+    val startActivityLauncher = rememberLauncherForActivityResult(contract = ActivityResultContracts.StartActivityForResult()) { result ->
+        // Handle the result of the activity here
+    }
+    viewModel.localNewsArticles.value = mutableListOf() // Clear the list of news articles
+
     Column(
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier
+            .fillMaxWidth()
             .padding(8.dp)
     ) {
         OutlinedTextField(
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
                 .padding(8.dp),
             value = searchQuery.value,
             onValueChange = { newValue -> searchQuery.value = newValue },
@@ -32,20 +53,36 @@ fun SearchScreen() {
         )
 
         Button(
-            onClick = { searchForQuery(searchQuery.value) },
+            onClick = {
+                // Call the API to search for news
+                viewModel.getNewsData(isTopHeadline = false, q = searchQuery.value, country = "in", category = "general", sortBy = "publishedAt")
+                      },
             modifier = Modifier.padding(top = 8.dp)
         ) {
             Text("Submit")
         }
 
-        // Display the results of the API call
-        // This will depend on the structure of your API and how you want to display the results
-        Text(text ="Results will be displayed here...: ${searchQuery.value}")
+        LazyColumn {
+            items(getArticlesByNewsType(viewModel, "for you")) { article ->
+                Box(modifier = Modifier.clickable {
+                    // Create an Intent to start the new activity
+                    val intent = Intent(context, SecondMainActivity::class.java)
+
+                    intent.putExtra("title", article.title)
+                    intent.putExtra("author", article.author)
+                    intent.putExtra("publishedAt", article.publishedAt)
+                    intent.putExtra("source", article.source.name)
+                    intent.putExtra("url", article.url)
+                    intent.putExtra("urlToImage", article.urlToImage)
+                    intent.putExtra("content", article.content)
+                    intent.putExtra("description", article.description)
+                    // Use the launcher to start the activity
+                    startActivityLauncher.launch(intent)
+                }){
+                    ArticleItem(article)
+                }
+            }
+        }
     }
 }
 
-fun searchForQuery(query: String) {
-    // Make an API call with the search query
-    // This will depend on the specifics of your API
-    // You might use a library like Retrofit to make the API call
-}
